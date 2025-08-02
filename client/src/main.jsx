1c7a3f09b8df469141ad42c61c019ecfbe063d9a
beingdeveloper21 <jhaprem4220@gmail.com>
2025-08-03 01:08:21 +0530

    changed auth file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { AppProvider } from './context/AppContext.jsx'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
@@ -13,7 +14,9 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')).render(
      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
      <BrowserRouter>
-      <App />
-      </BrowserRouter>,
+     <AppProvider>  
+        <App />
+      </AppProvider>
+      </BrowserRouter>
   </ClerkProvider>
 )
