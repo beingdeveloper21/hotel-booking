c89f27db00357817af9a9a43b366bbe0c90d0f47
beingdeveloper21 <jhaprem4220@gmail.com>
2025-08-03 10:46:10 +0530

    Changes in server.js

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(clerkMiddleware())
 
 //API to liten ClerkWebhooks
-app.use("/api/clerk",clerkWebhooks);
+app.post("/api/clerk/webhooks", express.json({ type: "*/*" }), clerkWebhooks);
 
 app.get('/',(req,res)=>res.send("API is Working"))
 app.use('/api/user',userRouter)
